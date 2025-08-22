62db660fb454213be69f0dceaefd781ddfaa82ac
Dhilipkumar <dhilip8778465332@gmail.com>
2025-08-22 12:14:49 +0530

    Python

---
+++ collection_module.py
@@ -0,0 +1,77 @@
+import collections
+
+
+# counter 
+
+# myList=["A","B","C","D","A","B","D"]
+
+# results=collections.Counter(myList)
+# print(results)
+
+# ordered dict
+# mydict=collections.OrderedDict()
+# mydict["a"]=1
+# mydict["b"]=3
+# mydict["c"]=4
+
+# for i , j in mydict.items():
+#     print(i,j)
+
+# mydict.pop("a")
+# print("\n")
+# for i , j in mydict.items():
+#     print(i,j)
+
+# mydict["a"]=4
+
+# print("\n")
+# for i , j in mydict.items():
+#     print(i,j)
+
+
+# default dict
+
+# mydict=collections.defaultdict(int)
+
+# myList=[1,2,3,1,1,21,3]
+
+# for i in myList:
+#     mydict[i]+=1
+
+
+# print(mydict)
+
+# chain map
+# student1={
+#     "name":"gowtham",
+#     "age":18
+  
+# }
+# student2={
+#     "name":"dhilip",
+#     "age":18
+  
+# }
+
+# results=collections.ChainMap(student1,student2)
+# print(results)
+
+# namedtuple
+
+# students=collections.namedtuple("Student",["name","age","course"])
+
+# student=students("vetri",18,"java")
+
+# print(student.name)
+
+
+# deque
+
+# myitems=collections.deque(["ball","bat","apple","watch","android","phone"])
+# print(myitems)
+
+# myitems.append("pencil")
+# print(myitems)
+
+# myitems.appendleft("pen")
+# print(myitems)
---
+++ packages/Draw/calculator.py
@@ -0,0 +1,22 @@
+def addition(add1,add2):
+    addition=add1+add2
+    print(addition)
+
+def subract(sub1,sub2):
+    subract=sub1-sub2
+    print(subract)
+
+def division(div1,div2):
+    division=div1/div2
+    print(division)
+
+def  power(num1,num2):
+    power=num1**num2
+    print(power)
+
+import math
+def gcd(num1,num2):
+    print(math.gcd())
+
+def factorial(num1):
+    print(math.factorial(num1))
